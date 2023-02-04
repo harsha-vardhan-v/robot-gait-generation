9b965ae130522d883dc83d02975a016fb87578e1
harsha-vardhan-v <v.harshavardhan93@gmail.com>
2023-02-04 07:45:28 +0000

    Created best_coordinate_walk.py file and added base code

---
+++ scripts/best_coordinate_walk.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+
+import rospy
+import os
+import tf
+import math
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Pose
+from gazebo_msgs.msg import ModelStates
+from gazebo_services import unload_controllers, load_controllers, call_spawn_model, call_delete_model
+
+final_position = 0
+best = [-3, -3, 4, 5, -4, 3, -3, 1, -5, 4, 2, 0, -4, 1, -5, 6, 4, -4, -2, -3, 5, -4, -3, -6, -3, 6, -1, -2, -3, 3, 1, 3, -3, -3, -6, 5, 3, 3, -1, -1, 5, 0, -3, -6, 1, 2, -5, -5, -3, 2, 4, -2, 1, -2, 4, 2, -2, 6, -2, 5, -6, -6, 1, -5, 5, -5, -6, -1, -2, -1, -6, -2, 5, 2, 5, 3, -6, 1, -1, 4, -4, 3, -4, -1, -6, 1, -2, 4, 6, -4, 0, -3, 4, -2, -2, -5, -4, -2, -1, -4, 6, 2, -1, 0, 0, 1, 0, -1, -6, -1, -3, -6, 3, 1, -2, 2, -4, -6, 4, 0, 2, -4, -4, 6, -1, 3, -6, -1]
+limit = 1.6
+
+def final_position_callback(data):
+    try:
+        global final_position 
+        final_position = math.sqrt(((data.pose[1].position.x)*(data.pose[1].position.x))+((data.pose[1].position.y)*(data.pose[1].position.y)))
+    except:
+        pass
+
+def main():
+    unload_controllers()
+    call_delete_model("rupert")
+
+    p = os.popen('rosrun xacro xacro ' + '~/Desktop/catkin_ws/src/rupert_learns/urdf/rupert.xacro')
+    xml_string = p.read()
+    p.close()
+
+    pose = Pose()
+    pose.position.x = 0
+    pose.position.y = 0
+    pose.position.z = 0.2
+    pose.orientation.x = 0
+    pose.orientation.y = 0
+    pose.orientation.z = 0
+    pose.orientation.w = 1
+
+    call_spawn_model(model_name='rupert',model_xml=xml_string,robot_namespace='',initial_pose=pose,reference_frame='world')
+    load_controllers()
+
+    hip1.publish(0)
+    hip2.publish(0)
+    hip3.publish(0)
+    hip4.publish(0)
+
+    rospy.loginfo('START')
+
+if __name__=='__main__':
+    rospy.init_node('best_coordinate_walk')
+    rospy.loginfo('Started best_coordinate_walk node')
+
+    rospy.wait_for_service('/gazebo/reset_world')
+    rospy.loginfo('STARTING /gazebo/reset_world')
+    rospy.wait_for_service('/gazebo/spawn_urdf_model')
+    rospy.wait_for_service('/gazebo/delete_model')
+    rospy.loginfo('STARTING /gazebo/delete_model')
+    rospy.loginfo('STARTING /gazebo/spawn_urdf_model')
+    rospy.wait_for_service('rupert/controller_manager/load_controller')
+    rospy.loginfo('STARTING rupert/controller_manager/load_controller')
+    rospy.wait_for_service('rupert/controller_manager/switch_controller')
+    rospy.loginfo('STARTING rupert/controller_manager/switch_controller')
+
+    hip1 = rospy.Publisher('/rupert/joint5_position_controller/command', Float64, queue_size=1)
+    hip2 = rospy.Publisher('/rupert/joint6_position_controller/command', Float64, queue_size=1)
+    hip3 = rospy.Publisher('/rupert/joint7_position_controller/command', Float64, queue_size=1)
+    hip4 = rospy.Publisher('/rupert/joint8_position_controller/command', Float64, queue_size=1)
+    knee1 = rospy.Publisher('/rupert/joint1_position_controller/command', Float64, queue_size=1)
+    knee2 = rospy.Publisher('/rupert/joint2_position_controller/command', Float64, queue_size=1)
+    knee3 = rospy.Publisher('/rupert/joint3_position_controller/command', Float64, queue_size=1)
+    knee4 = rospy.Publisher('/rupert/joint4_position_controller/command', Float64, queue_size=1)
+    ankle1 = rospy.Publisher('/rupert/joint9_position_controller/command', Float64, queue_size=1)
+    ankle2 = rospy.Publisher('/rupert/joint10_position_controller/command', Float64, queue_size=1)
+    ankle3 = rospy.Publisher('/rupert/joint11_position_controller/command', Float64, queue_size=1)
+    ankle4 = rospy.Publisher('/rupert/joint12_position_controller/command', Float64, queue_size=1)
+    
+    rospy.loginfo('Created publishers')
+
+    rospy.Subscriber('/gazebo/model_states', ModelStates, final_position_callback)
+    rospy.loginfo('Created subscribers')
+
+    main()
